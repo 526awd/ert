6b60d5495d9784c943e4a9d60113dae4d9010d95
526awd <62787078+526awd@users.noreply.github.com>
2026-08-19 08:27:00 +0800

    Modify loop range and simplify status handling
    Updated the range in the loop and removed the bad status logging.

--- main.py
+++ main.py
@@ -2,12 +2,11 @@ import requests as rt
 f=open("a\\a.txt","a+",encoding="utf8")
 d=open("a\\d.txt","a+",encoding="utf8")
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36'}
-for i in range(0,100000000):
+for i in range(11000,1000000):
     e="http://cdn.sinacloud.net/edge.v.iask.com/%s.hlv"%i
     ad=rt.get(e,headers=headers)
     if ad.status_code==404:
-        print(e+" bad")
-        d.write(e+" bad\n")
+        pass
     else:
         print(e+" pass")
         f.write(e+"\n")
